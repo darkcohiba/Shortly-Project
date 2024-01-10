d15156df0dab337cc3b91cec3acff22466ce8033
Sam Waters <samuel.waters@gmail.com>
2024-01-10 15:24:03 -0700

    chapter: shorten section javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './style.css'
+
+const input = document.getElementById('link-input')
+const linkForm = document.getElementById('link-form')
+const errMsg = document.getElementById('err-msg')
+
+linkForm.addEventListener('submit', formSubmit)
+
+function formSubmit(e){
+    e.preventDefault()
+
+    if (input.value === ""){
+        errMsg.textContent = "Field cannot be empty"
+        input.classList.add('border-red')
+    }else{
+        errMsg.textContent = ""
+        input.classList.remove('border-red')
+    }
+}
