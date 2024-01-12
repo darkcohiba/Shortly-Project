d28616402698e75fa8103704d23e684e90fe4169
Sam Waters <samuel.waters@gmail.com>
2024-01-11 19:40:38 -0700

    finished proj

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import './style.css'
 
+const btn = document.getElementById('menu-btn')
+const menu = document.getElementById('menu')
+
+
 const input = document.getElementById('link-input')
 const linkForm = document.getElementById('link-form')
 const errMsg = document.getElementById('err-msg')
 
+btn.addEventListener('click', navToggle)
 linkForm.addEventListener('submit', formSubmit)
 
 function formSubmit(e){
@@ -16,4 +21,12 @@ function formSubmit(e){
         errMsg.textContent = ""
         input.classList.remove('border-red')
     }
+}
+
+function navToggle(){
+    btn.classList.toggle('open')
+    menu.classList.toggle('flex')
+    menu.classList.toggle('hidden')
+
+
 }
